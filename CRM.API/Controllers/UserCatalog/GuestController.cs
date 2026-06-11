d22a8c3601714b0222d297caf482577ea03161d8
Genshirog <imbaxgx1fromyt@gmail.com>
2026-06-11 15:26:38 +0800

    fixed the guest inquiry

---
+++ CRM.API/Controllers/UserCatalog/GuestController.cs
@@ -0,0 +1,17 @@
+﻿using CRM.API.Controllers;
+using CRM.Core.DTOs.Users;
+using CRM.Core.Entities;
+using CRM.Core.Services.UserCatalog;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.API.Controllers.UserCatalog;
+
+[ApiController]
+[Route("api/guest")]
+public class GuestController(IGuestService service) : BaseController<GuestResponseDto, CreateGuestDto, UpdateGuestDto>(service)
+{
+    [HttpGet ("/email")]
+    public async Task<IActionResult> GetByEmail(string email) => Ok(await service.GetByEmailAsync(email));
+    [HttpGet ("/phoneNumber")]
+    public async Task<IActionResult> GetByPhoneNumber(string phoneNumber) => Ok(await service.GetByPhoneNumberAsync(phoneNumber));
+}
